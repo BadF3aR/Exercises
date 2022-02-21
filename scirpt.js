65e5e775a11d854d1d9527cb22a478b1f4945f7c
BadF3aR <cziken99@gmail.com>
2022-02-21 17:37:28 +0100

    update 17;37

--- scirpt.js
+++ scirpt.js
@@ -1,1 +1,15 @@
 console.log("Hello world")
+
+//Operatory arytemetyczne 
+
+var x = 2;
+
+console.log(x + 5);
+console.log(x - 5);
+console.log(x * 5);
+console.log(x / 5);
+console.log(5 % 2); //reszta z dzielenia 
+
+console.log("5" + "tekst 23"); //konkatenacja 
+
+
