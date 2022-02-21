8b3c9ee6a4a0a3051b62e005bb07cd313252479e
BadF3aR <cziken99@gmail.com>
2022-02-21 20:00:45 +0100

    update 20;00

--- scirpt.js
+++ scirpt.js
@@ -12,4 +12,22 @@ console.log(5 % 2); //reszta z dzielenia
 
 console.log("5" + "tekst 23"); //konkatenacja 
 
+console.log("5" * 2);
+console.log("5" * "2"); // js rozpoznaje cyfry
+
+console.log(2 + 2 * 2);
+console.log((2 + 2) * 2); //nawias zmienia kolejność wykonywania działań 
+
+console.log("x = " + x );
+
+x = x + 5;
+
+console.log(x)
+
+x += 5; // to jest to samo co wyzej tylko skrócony zapis 
+
+console.log(x)
+
+
+
 
