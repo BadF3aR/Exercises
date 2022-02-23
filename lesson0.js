5764cef223be8b7d78f15461fab28ef4b0b2f72e
BadF3aR <cziken99@gmail.com>
2022-02-23 20:55:37 +0100

    update 22:55

---
+++ lesson0.js
@@ -0,0 +1,36 @@
+// Operatory porównania 
+
+var x = 5 == 5;
+console.log(x);
+console.log(typeof(x));
+
+console.log("-----");
+
+console.log(5 == 5);
+console.log(5 == 5);
+
+console.log(5 == "5");
+console.log(5 === "5"); // 3*= sprawdz rowniez typ danych 
+
+console.log("-----");
+console.log(5 != 5);
+console.log(5 != 0);
+
+console.log(5 != "5");
+console.log(5 !== "5");
+
+console.log("----");
+
+console.log(5 > 5);
+console.log(5 >= 5);
+
+console.log(5 < 5);
+console.log(5 <= 5);
+
+// Operatory logiczne
+
+console.log(5 >= 5 || 5 == 1);  //  || znaczy or  
+
+console.log(true && false);     // and wymaga 2x true
+
+console.log(!5 > 0);            // not operator negacji zaprzeczenia
