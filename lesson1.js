f2bc2322015c151f51b81fd4ef68ea9d94ced336
BadF3aR <cziken99@gmail.com>
2022-02-24 13:59:17 +0100

    update 13;59

--- lesson1.js
+++ lesson1.js
@@ -2,4 +2,53 @@
 
 //Instrukcje warunkowe
 
-document.getElementById("A").innerHTML = "Hello World";
+// document.getElementById("A").innerHTML = "Hello World";
+
+var A = document.getElementById("A");
+
+var x = 5, y = 10;
+var age = 19;
+var num = 0;
+
+
+age >= 18 ?   alert("Pelnoletni") : alert("niepelnoletni");    // instrukcja warunkowa troj argumentowa 
+
+
+/* if (age >= 18)
+{
+    A.innerHTML = "Pełnoletni";
+}
+else
+{
+    A.innerHTML = "Niepełnoletni";
+} */
+
+/* if (num > 0)
+{
+    A.innerHTML = "Dodatnia";
+}
+else                                //zagniezdzenie instrukcji warunkowej 
+{
+    if (num == 0)
+    {
+        A.innerHTML = "Rowna 0";
+    }
+    else
+    {
+        A.innerHTML = "Ujemna";
+    }
+} */
+
+if (num > 0)
+{
+    A.innerHTML = "Dodatnia";
+}
+else if (num == 0)
+{
+   A.innerHTML = "Rowna 0";
+}
+else
+{
+    A.innerHTML = "ujemna";
+}
+
