c0ef0c79600264097e71026498e34b03bb9741ae
BadF3aR <cziken99@gmail.com>
2022-03-09 21:02:38 +0100

    update 21;02

--- lesson4.js
+++ lesson4.js
@@ -1,12 +1,27 @@
 // Instrukcje skoku
 
 var box = document.getElementById("result");
-var box2 = document.getElementById("result1");
+var box1 = document.getElementById("result1");
 
 // Continue 
-
-for (var i = 0; i <= 10; i++)
+for (j = 0; j < 3; j++)
 {
-    box.innerHTML += i + "<br>";
+    for (var i = 0; i <= 10; i++)
+    {
+        if (i % 2 == 1)
+        {
+            continue;
+        }
+        box.innerHTML += j + " - " + i + "<br>";
+    }
 }
 // Break
+
+for (var i = 0; i <= 10;  i++)
+{
+    if( i == 5)
+    {
+        break;
+    }
+    box1.innerHTML += i + "<br>";
+}
