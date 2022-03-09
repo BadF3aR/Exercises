357700a1a2f0ff14b6e44f585f4b206c11513aa7
BadF3aR <cziken99@gmail.com>
2022-03-09 20:52:04 +0100

    update 20;52

---
+++ lesson4.js
@@ -0,0 +1,12 @@
+// Instrukcje skoku
+
+var box = document.getElementById("result");
+var box2 = document.getElementById("result1");
+
+// Continue 
+
+for (var i = 0; i <= 10; i++)
+{
+    box.innerHTML += i + "<br>";
+}
+// Break
