f2eeef2653e895ef1e2190eebb9a48d45d570f90
BadF3aR <cziken99@gmail.com>
2022-03-09 23:22:08 +0100

    update 23;22

--- lesson4.js
+++ lesson4.js
@@ -4,10 +4,19 @@ var box = document.getElementById("result");
 var box1 = document.getElementById("result1");
 
 // Continue 
-for (j = 0; j < 3; j++)
+
+
+loop1:
+for (j = 0; j < 3; j++) 
 {
-    for (var i = 0; i <= 10; i++)
+    loop2:
+    for (var i = 0; i <= 10; i++) //petla w petli "label"
     {
+        box.innerHTML += " - <br>"
+        if(i > 5)
+        {
+            continue loop1;         //przenosi do koknretnego obiegu/petli
+        }
         if (i % 2 == 1)
         {
             continue;
