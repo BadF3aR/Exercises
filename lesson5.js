dd2dc5755e3a6a24581a0984feea8292ac6ce9f1
BadF3aR <cziken99@gmail.com>
2022-03-10 19:41:01 +0100

    update 19;40

--- lesson5.js
+++ lesson5.js
@@ -1,3 +1,29 @@
 // Funkcje
 
 var result = document.getElementById("result");
+
+add(5);
+add(10, 4);
+add(5, 10, 15);
+
+function add(a, b) 
+{
+    if (typeof(b) === "undefined")
+    {
+        b = 0;
+    }
+    if (arguments.length < 3)
+        result.innerHTML += a + "+" + b + "=" + (a + b) + "<br>"; 
+    else
+        result.innerHTML += a + "+" + b + "+" + arguments[2] + "=" + (a + b + arguments[2]) + "<br>"; 
+
+}
+
+
+//hello();
+
+function hello()
+{
+    alert("Siemano!!");
+}
+
