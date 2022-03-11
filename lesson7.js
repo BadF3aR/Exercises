f01ddf5d1e489bbc5d429eb7638170a3e46f50ea
BadF3aR <cziken99@gmail.com>
2022-03-11 18:54:26 +0100

    18;54

--- lesson7.js
+++ lesson7.js
@@ -1,5 +1,7 @@
 var result = document.getElementById("result");
 
+// 1 sposob
+
 var tab = new Array("Poniedziałek", "Wtorek", "Środa");
 
 result.innerHTML += tab[0] + "<br>";
@@ -21,3 +23,26 @@ for (var i = 0; i < tab.length; i++)
 {
     result.innerHTML += tab[i] + "<br>";
 }
+
+tab.forEach(x => console.log(x));
+
+//inny sposob 
+
+var tab2 = new Array(3);
+tab2[0] = 10;
+tab2[1] = true;
+tab2[2] = "JS";
+
+console.log(tab2);
+
+// 3 sposob
+
+var tab3 = ["wiosna", "lato", "jesien", "zima"];
+
+console.log(tab3);
+
+//łaczenie tablic
+
+var tab4 = tab.concat(tab2).concat(tab3);
+
+console.log(tab4);
