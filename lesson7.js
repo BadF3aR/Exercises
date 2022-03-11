82fc1e3407490cb0bbe8e98218d3b68e863d3c07
BadF3aR <cziken99@gmail.com>
2022-03-11 18:43:08 +0100

    update 18;43

--- lesson7.js
+++ lesson7.js
@@ -4,8 +4,20 @@ var tab = new Array("Poniedziałek", "Wtorek", "Środa");
 
 result.innerHTML += tab[0] + "<br>";
 tab[3] = "Czwartek";
+tab[4] = "Piątek";
+tab[5] = "Sobota";
 result.innerHTML += tab[3] + "<br>";
+tab[6] = "Niedziela";
+
+result.innerHTML += "Długość = " + tab.length + "<br>";
 
 
+console.log(tab);
 
-console.log(tab) + "<br>";
+
+result.innerHTML += "----------<br>"
+
+for (var i = 0; i < tab.length; i++)
+{
+    result.innerHTML += tab[i] + "<br>";
+}
