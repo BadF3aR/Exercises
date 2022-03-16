840ac2a4c898948e81758c0546af25b946a0d5a7
BadF3aR <cziken99@gmail.com>
2022-03-16 18:30:13 +0100

    update 18;30

--- lesson10.js
+++ lesson10.js
@@ -12,5 +12,42 @@ button.addEventListener("click", sprawdz);
 
 function sprawdz()
 {
-    answer.innerHTML = input.value;
+    var value = Number(input.value);
+    if (Number.isInteger(value) && value >= 1 && value <= 10)
+    {
+        i++;
+
+        if(los > value)
+        {
+            answer.innerHTML = "Wylosowana liczba jest większa od Twojej!";
+        }
+        else if(los < value)
+        {
+            answer.innerHTML = "Wylosowana liczba jest mniejsza od Twojej!";
+        }
+        else
+        {
+            answer.innerHTML = "Gratulacje! Zgadłeś liczbę za " + i + " razem!";
+            answer.classList.add("good");
+            button.innerHTML ="Odśwież";
+            button.removeEventListener("click", sprawdz);
+            button.addEventListener("click", odswiez);
+        }
+
+
+        var li = document.createElement("li");
+        list.appendChild(li);
+        list.lastChild.innerHTML =  i.toString() + ": " + value;
+    }
+    else
+    {
+        alert("Błędna liczba");
+    }
 }
+
+function odswiez()
+{
+    location.reload();
+}
+
+//11;52 skonczyłem
