97d073639095f1504726e904e4fa87e4c40a9555
BadF3aR <cziken99@gmail.com>
2022-03-16 15:27:38 +0100

    update 15;27

---
+++ lesson10.js
@@ -0,0 +1,5 @@
+var input = document.getElementById("input")
+var button = document.getElementById("button")
+var answer = document.getElementById("answer")
+var list = document.getElementById("list")
+var i = 0;
