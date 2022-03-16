9bfe81bc05cef89dd9920852973bcec1bc6f91b0
BadF3aR <cziken99@gmail.com>
2022-03-16 20:10:46 +0100

    update 20;10

--- lesson11.js
+++ lesson11.js
@@ -9,3 +9,34 @@ placeholder.innerHTML = "<b>JavaScript</b>";
 var goods = document.getElementsByClassName("good");
 console.log(goods);
 
+for ( var i = 0; i < goods.length; i++)
+{
+    goods[i].innerText += " - OK ";
+}
+//----------------------------------------------- dodawanie
+var li = document.getElementsByTagName("li");
+console.log(li);
+
+var newLi = document.createElement("li");
+var text = document.createTextNode("Tekst 5");
+newLi.appendChild(text);
+
+var list = document.getElementById("list");
+list.appendChild(newLi);
+//-------------------------------------------------
+var newLi2 = document.createElement("li");
+var text2 = document.createTextNode("Tekst 3.5");
+newLi2.appendChild(text2);
+
+list.insertBefore(newLi2, document.getElementById("li4"));
+
+//-------------------------------------------------- modyfikacja 
+
+var firstLi = list.firstElementChild;
+
+var newLi3 = document.createElement("li");
+var text3 = document.createTextNode("Nagłówek");
+newLi3.appendChild(text3);
+newLi3.classList.add("good");
+
+list.replaceChild(newLi3, firstLi);
