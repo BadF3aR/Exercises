1d654d00a6e71c9762a9e937735cfc702f527f20
BadF3aR <cziken99@gmail.com>
2022-03-16 19:42:43 +0100

    update 19;42

---
+++ lesson11.js
@@ -0,0 +1,11 @@
+// DOM
+
+console.log(document);
+
+var placeholder = document.getElementById("placeholder");
+//placeholder.innerText = "<b>JavaScript</b>"; 
+placeholder.innerHTML = "<b>JavaScript</b>"; 
+
+var goods = document.getElementsByClassName("good");
+console.log(goods);
+
