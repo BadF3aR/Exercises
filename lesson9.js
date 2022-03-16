1f11330a096d89ae3692f77321b545dec128a398
BadF3aR <cziken99@gmail.com>
2022-03-16 14:14:42 +0100

    update 14;14

---
+++ lesson9.js
@@ -0,0 +1,11 @@
+//Math i Date
+
+var math = document.getElementById("math");
+var date = document.getElementById("date");
+
+//Math
+
+var liczba = prompt("Podaj liczbę:" , "0");
+math.innerHTML += "Liczba:" + liczba + "<br>";
+
+//Date
