440af23250c121fac07ab35274fda58d14590bad
BadF3aR <cziken99@gmail.com>
2022-03-16 15:31:18 +0100

    update 15;31

--- lesson10.js
+++ lesson10.js
@@ -3,3 +3,6 @@ var button = document.getElementById("button")
 var answer = document.getElementById("answer")
 var list = document.getElementById("list")
 var i = 0;
+
+var los = Math.floor(Math.random() * 10) + 1;
+//alert(los);
